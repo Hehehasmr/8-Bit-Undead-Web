26178d54f11e998bbd8d3fcf401c1638147e538a
Hehehasmr <pro1234plays@gmail.com>
2025-02-27 18:12:59 -0700

    Update game.js

--- game.js
+++ game.js
@@ -3,16 +3,10 @@ console.log("Game.js is running!");
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM fully loaded and parsed");
 
-    // Global variables
-    let player, cursors, bullets, zombies, health, score, ammo, wave, money;
+    let player, cursors, bullets, health, score, ammo, wave, money;
     let lastShot = 0;
     let weaponLevel = 1;
-    let shopItems = [
-        { name: "Fire Rate Boost", cost: 50, effect: () => weaponLevel++ },
-        { name: "More Ammo", cost: 30, effect: () => ammo += 10 },
-        { name: "Health Pack", cost: 40, effect: () => health += 20 },
-    ];
-
+    
     class StartMenu extends Phaser.Scene {
         constructor() { super("StartMenu"); }
 
@@ -51,7 +45,6 @@ document.addEventListener("DOMContentLoaded", function () {
             this.player = this.physics.add.sprite(400, 300, 'player').setCollideWorldBounds(true);
             cursors = this.input.keyboard.createCursorKeys();
             bullets = this.physics.add.group({ defaultKey: 'bullet' });
-            zombies = this.physics.add.group();
             health = (this.difficulty === "easy") ? 100 : 50;
             ammo = 10;
             wave = 1;
@@ -67,24 +60,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
             this.input.keyboard.on("keydown-SPACE", () => this.fireBullet());
             this.input.keyboard.on("keydown-R", () => this.reload());
-
-            this.physics.add.overlap(bullets, zombies, this.killZombie, null, this);
-            this.physics.add.overlap(this.player, zombies, this.takeDamage, null, this);
-        }
-
-        update() {
-            this.player.setVelocity(0);
-            if (cursors.left.isDown) this.player.setVelocityX(-160);
-            if (cursors.right.isDown) this.player.setVelocityX(160);
-            if (cursors.up.isDown) this.player.setVelocityY(-160);
-            if (cursors.down.isDown) this.player.setVelocityY(160);
-            zombies.children.iterate(zombie => this.physics.moveToObject(zombie, this.player, 50));
-
-            if (zombies.countActive() === 0) {
-                wave++;
-                this.waveText.setText(`Wave: ${wave}`);
-                this.spawnWave();
-            }
         }
 
         fireBullet() {
@@ -109,15 +84,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 let zombie = this.physics.add.sprite(Phaser.Math.Between(50, 750), Phaser.Math.Between(50, 550), 'zombie');
                 zombies.add(zombie);
             }
-        }
-
-        killZombie(bullet, zombie) {
-            bullet.destroy();
-            zombie.destroy();
-            score += 10;
-            money += 5;
-            this.scoreText.setText(`Score: ${score}`);
-            this.moneyText.setText(`Money: $${money}`);
         }
     }
 
